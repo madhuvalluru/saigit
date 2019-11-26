053faed73de475b287dd21217bbcd71aedf3f43b
madhu <50739066+valllurumadhu@users.noreply.github.com>
2019-11-26 14:36:00 +0530

    database changed

--- sai/sai/settings.py
+++ sai/sai/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'sai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sai',
-        'USER':'root',
-        'PASSWORD':'madhu123',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
